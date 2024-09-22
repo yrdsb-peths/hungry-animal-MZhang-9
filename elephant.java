5d1ded30108acba676d8309a10243cc01cc777aa
MZhang-9 <m2volleyballa@gmail.com>
2024-09-21 21:09:42 -0400

    added sound and background

--- elephant.java
+++ elephant.java
@@ -8,10 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class elephant extends Actor
 {
-    /**
-     * Act - do whatever the elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage idle = new GreenfootImage("
     public void act()
     {
         if(Greenfoot.isKeyDown("left")){
@@ -28,6 +26,7 @@ public class elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
             world.increaseScore();
+            elephantSound.play();
         }
     }
 }
