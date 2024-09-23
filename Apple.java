04f4b9f8d1ff22735d7f1239cc2a740068bae73a
MZhang-9 <m2volleyballa@gmail.com>
2024-09-23 08:11:26 -0400

    finished everything added speed

--- Apple.java
+++ Apple.java
@@ -12,10 +12,11 @@ public class Apple extends Actor
      * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int speed = 1;
     public void act()
     {
         int x = getX();
-        int y = getY() + 2;
+        int y = getY() + speed;
         setLocation(x, y);
         MyWorld world = (MyWorld) getWorld();
         if(getY() >= world.getHeight() - 1)
@@ -23,5 +24,9 @@ public class Apple extends Actor
             world.gameOver();
             world.removeObject(this);
         }
+    }
+    public void setSpeed(int spd)
+    {
+        speed = spd;
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -6,6 +6,8 @@ public class MyWorld extends World
 
     public int score = 0;
     Label scoreLabel;
+    int level = 1;
+    
     public MyWorld()
     {    
         super(600, 400, 1);
@@ -26,10 +28,14 @@ public class MyWorld extends World
     public void increaseScore(){
         score++;
         scoreLabel.setValue(score);
+        if(score%5 == 0){
+            level += 1;
+        }
     }
     
     public void createApple(){
         Apple apple = new Apple();
+        apple.setSpeed(level);
         int x = Greenfoot.getRandomNumber(600);
         addObject(apple, x, 0);
         
